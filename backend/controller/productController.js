09227fbf496b2444543129b7188341884c1ec6b6
Abishek Khanna M S <75411526+Abishekkhanna@users.noreply.github.com>
2025-11-19 21:25:29 +0530

    Filter and Pagination Function

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -12,13 +12,30 @@ export const addProduct = async (req, res) => {
 };
 
 // Get all product
-export const getAllProducts = async (req, res) => {
+export const getAllProducts = async (req, res, next) => {
   // const product = await Product.find();
-  const apihelper = new APIhelper(Product.find(), req.query).Search();
+  const resultPerPage = 4;
+  const apihelper = new APIhelper(Product.find(), req.query).Search().Filter();
+  const filteredQuery = apihelper.query.clone();
+  const productCount = await filteredQuery.countDocuments();
+
+  const totalPages = Math.ceil(productCount / resultPerPage);
+  const page = Number(req.query.page) || 1;
+
+  if (totalPages > 0 && page > totalPages) {
+    return next(new HandleError("This page doesn't Exist", 404));
+  }
+
+  apihelper.Pagination(resultPerPage);
+
   const product = await apihelper.query;
   res.status(200).json({
     success: true,
     product,
+    productCount,
+    resultPerPage,
+    totalPages,
+    currentPage: page,
   });
 };
 
--- backend/helper/APIhelper.js
+++ backend/helper/APIhelper.js
@@ -15,7 +15,19 @@ class APIhelper {
     this.query = this.query.find({ ...keyword });
     return this;
   }
-  Filter() {}
+  Filter() {
+    const queryCopy = { ...this.queryStr };
+    const removedFields = ["keyword", "page", "limit"];
+    removedFields.forEach((key) => delete queryCopy[key]);
+    this.query = this.query.find(queryCopy);
+    return this;
+  }
+  Pagination(resultPerPage) {
+    const currentPage = Number(this.queryStr.page) || 1;
+    const skip = resultPerPage * (currentPage - 1);
+    this.query = this.query.limit(resultPerPage).skip(skip);
+    return this;
+  }
 }
 
 export default APIhelper;
